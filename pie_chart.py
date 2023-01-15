932853f4a0bc31e7d9341645c39da1807bd00450
Agnieszka <63312045+agafrac@users.noreply.github.com>
2023-01-15 21:08:55 +0100

    Add pie chart (Highcharts) file

---
+++ pie_chart.py
@@ -0,0 +1,88 @@
+import justpy as jp
+
+import pandas
+
+
+data = pandas.read_csv("/Users/agnieszka/Desktop/pandas/reviews.csv", parse_dates=['Timestamp'])
+share = data.groupby(['Course Name'])['Rating'].count()
+
+chart_def = """
+{
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Browser market shares in May, 2020',
+        align: 'left'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    accessibility: {
+        point: {
+            valueSuffix: '%'
+        }
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+            }
+        }
+    },
+    series: [{
+        name: 'Brands',
+        colorByPoint: true,
+        data: [{
+            name: 'Chrome',
+            y: 70.67,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Edge',
+            y: 14.77
+        },  {
+            name: 'Firefox',
+            y: 4.86
+        }, {
+            name: 'Safari',
+            y: 2.63
+        }, {
+            name: 'Internet Explorer',
+            y: 1.53
+        },  {
+            name: 'Opera',
+            y: 1.40
+        }, {
+            name: 'Sogou Explorer',
+            y: 0.84
+        }, {
+            name: 'QQ',
+            y: 0.51
+        }, {
+            name: 'Other',
+            y: 2.6
+        }]
+    }]
+}
+"""
+
+
+def app():
+    wp = jp.QuasarPage()
+    h1 = jp.QDiv(a=wp, text="Analysis of Course Reviews", classes="text-h3 q-pa-md text-center")
+    p1 = jp.QDiv(a=wp, text="These graphs represents course review analysis", classes="text-h5 q-pa-md")
+    hc = jp.HighCharts(a=wp, options=chart_def)
+    hc.options.title.text = "Average temperature"
+    hc_data = [{"name": v1, "y": v2} for v1, v2 in zip(share.index, share)]
+    hc.options.series[0].data = hc_data
+    return wp
+
+
+jp.justpy(app)
